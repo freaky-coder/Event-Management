b43411cc838bf98a2e209455a8c84f48f125a7f5
freaky-coder <ak246@snu.edu.in>
2017-11-04 09:27:52 +0530

    Code Updated

--- src/eventmanagement/EventManagement.java
+++ src/eventmanagement/EventManagement.java
@@ -16,6 +16,8 @@ public class EventManagement {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-    }
+        System.out.println("*****Welcome to Event Management portal*****");
+        System.out.println("\t Now Manage events at ease");
+                }
     
 }
