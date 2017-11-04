42fe5a5e7bd3675de455f8670510566f4582c999
freaky-coder <ak246@snu.edu.in>
2017-11-04 09:35:44 +0530

    Code Updated

--- src/eventmanagement/EventManagement.java
+++ src/eventmanagement/EventManagement.java
@@ -5,6 +5,8 @@
  */
 package eventmanagement;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Ayush
@@ -16,8 +18,35 @@ public class EventManagement {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        int choice;
+        Scanner sc = new Scanner(System.in);
         System.out.println("*****Welcome to Event Management portal*****");
         System.out.println("\t Now Manage events at ease");
+        System.out.println("Tell us what to do....");
+        System.out.println("1. Register a User");
+        System.out.println("2. Create a Team");
+        System.out.println("3. Create an Event");
+        System.out.println("4. Check Winner of previous event");
+        choice= sc.nextInt();
+        switch(choice)
+        {
+            case 1:
+            {
+                
+            }
+            case 2:
+            {
+                
+            }
+            case 3:
+            {
+                
+            }
+            case 4:
+            {
+                
+            }
+        }
                 }
     
 }
