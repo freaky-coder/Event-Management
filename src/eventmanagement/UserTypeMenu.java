724571fc644bd721e3edd3b778aeee48f945eaee
freaky-coder <ak246@snu.edu.in>
2017-11-07 09:10:36 +0530

    Menu Added

--- src/eventmanagement/UserTypeMenu.java
+++ src/eventmanagement/UserTypeMenu.java
@@ -13,4 +13,23 @@ package eventmanagement;
 // This Class will contain all the internal menu and method for each and every type of user
 public class UserTypeMenu {
     
+    public void normalMenu()
+    {
+        // Menu given to a normal user
+    }
+    
+    public void volunteerMenu()
+    {
+      // Menu given to a volunteer  
+    }
+    
+    public void managerMenu()
+    {
+        // Menu provided to a Manager
+    }
+    
+    public void adminMenu()
+    {
+        // Menu for an Admin
+    }
 }
